b34bf28ec734876609a8c9675d31375f846f0ae6
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2022-01-12 01:45:50 +0530

    Corrected rat in a maze Lec 40

--- Recursion/rat_in_maze.cpp
+++ Recursion/rat_in_maze.cpp
@@ -4,66 +4,92 @@
 #define endl '\n'
 using namespace std;
 
-bool possible(vector<vector<int>> &a, vector<vector<int>> visited,int x,int y,int n){
 
-    if((x>=0 && x<a.size()) && (y>=0 && y<a.size()) && visited[x][y]==0 && a[x][y]==1){
-        return 1;
-    }else{
-        return 0;
-    }
-}
+// METHOD 1:
 
+void rat_in_maze(vector<vector<int>> &a, vector<vector<int>> &visited, vector<string> &ans, string path ,int i, int j, int n){
 
-void rat_in_maze(vector<vector<int>> &a, vector<vector<int>> visited, vector<string> &ans, string path ,int x, int y, int n){
-
-    if(x==n-1 && y==n-1){
+    if(i==n-1 && j==n-1){
         ans.push_back(path);
         return;
     }
 
-    visited[x][x] = 1;
+    visited[i][j] = 1;
 
-    int xx = x, yy = y+1;                           // right  --> i,j+1
-    if(possible(a,visited,xx,yy,n)){
-        path.push_back('R');
-        rat_in_maze(a,visited,ans,path,xx,yy,n);
-        path.pop_back();
+    if(j+1<n && a[i][j+1]==1 && visited[i][j+1]==0){         // for right  i,j+1
+        rat_in_maze(a,visited,ans,path+'R',i,j+1,n);
     }
 
-    xx = x+1, yy = y;                               // down  --> i+1,j
-    if(possible(a,visited,xx,yy,n)){
-        path.push_back('D');
-        rat_in_maze(a,visited,ans,path,xx,yy,n);
-        path.pop_back();
+    if(j-1>=0 && a[i][j-1]==1 && visited[i][j-1]==0){    // for left  i,j-1
+        rat_in_maze(a,visited,ans,path+'L',i,j-1,n);
     }
 
-    xx = x, yy = y-1;                               // left  --> i,j-1
-    if(possible(a,visited,xx,yy,n)){
-        path.push_back('L');
-        rat_in_maze(a,visited,ans,path,xx,yy,n);
-        path.pop_back();
+    if(i-1>=0 && a[i-1][j]==1 && visited[i-1][j]==0){    // for up  i-1,j
+        rat_in_maze(a,visited,ans,path+'U',i-1,j,n);
     }
 
-    xx = x-1, yy = y;                               // up  --> i-1,j
-    if(possible(a,visited,xx,yy,n)){
-        path.push_back('U');
-        rat_in_maze(a,visited,ans,path,xx,yy,n);
-        path.pop_back();
+    if(i+1<n && a[i+1][j]==1 && visited[i+1][j]==0){    // for down  i+1,j
+        rat_in_maze(a,visited,ans,path+'D',i+1,j,n);
     }
 
-    visited[x][y] = 0;
-
+    visited[i][j] = 0;
 }
+
+
+// METHOD 2:
+
+// bool possible(vector<vector<int>> &a, vector<vector<int>> &visited,int x,int y,int n){
+
+//     if((x>=0 && x<n) && (y>=0 && y<n) && visited[x][y]==0 && a[x][y]==1){
+//         return 1;
+//     }else{
+//         return 0;
+//     }
+// }
+
+
+// void rat_in_maze(vector<vector<int>> &a, vector<vector<int>> &visited, vector<string> &ans, string path ,int x, int y, int n){
+
+//     if(x==n-1 && y==n-1){
+//         ans.push_back(path);
+//         return;
+//     }
+
+//     visited[x][y] = 1;
+
+
+//     // right  --> i,j+1
+//     if(possible(a,visited,x,y+1,n)){
+//         rat_in_maze(a,visited,ans,path+'R',x,y+1,n);
+//     }
+
+//     // down  --> i+1,j
+//     if(possible(a,visited,x+1,y,n)){
+//         rat_in_maze(a,visited,ans,path+'D',x+1,y,n);
+//     }
+
+//     // left  --> i,j-1
+//     if(possible(a,visited,x,y-1,n)){
+//         rat_in_maze(a,visited,ans,path+'L',x,y-1,n);
+//     }
+
+//     // up  --> i-1,j
+//     if(possible(a,visited,x-1,y,n)){
+//         rat_in_maze(a,visited,ans,path+'U',x-1,y,n);
+//     }
+
+//     visited[x][y] = 0;
+
+// }
 
 void solve(){
     
     int n;cin>>n;
-    vector<vector<int>> a(n,vector<int> (n)), visited(n,vector<int> (n));
+    vector<vector<int>> a(n,vector<int> (n)), visited(n,vector<int> (n,0));
 
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n; j++){
             cin>>a[i][j];
-            visited[i][j] = 0;
         }
     }
 
@@ -74,7 +100,7 @@ void solve(){
     vector<string> ans;
     string path = "";
     rat_in_maze(a,visited,ans,path,0,0,n);
-
+    sort(ans.begin(),ans.end());
     for(auto it:ans){
         cout<<it<<endl;
     }
