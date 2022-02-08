98b2d2a409e508ca9b5ccecbe8ed4e283ef2347e
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2022-02-08 22:16:36 +0530

    Lec 43 OOPS part 2

---
+++ OOPS/Encapsulation.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define fast_io std::ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+using namespace std;
+
+class Student{
+
+private:
+    string name;
+    int age;
+    int height;
+
+public:
+    int getAge(){
+        return this->age;
+    }
+};
+
+
+void solve(){
+    Student first;
+    cout<<first.getAge()<<endl;
+}
+
+int main()
+{
+    fast_io;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    // Code
+    int t = 1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
