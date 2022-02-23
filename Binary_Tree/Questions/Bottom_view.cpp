5dd73c1762b730124f3adb4540307b9327b3e941
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2022-02-23 23:49:33 +0530

    Lec 64 Binary Tree Ques

---
+++ Binary_Tree/Questions/Bottom_view.cpp
@@ -0,0 +1,46 @@
+// https://practice.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1/
+
+/** Approach: TC = O(n)  SC = O(n)  **/
+
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        vector<int> ans;
+        if(root==NULL) return ans;
+        
+        map<int,int> topNode;
+        queue<pair<Node*, int>> q;
+        q.push(make_pair(root,root->data));
+        
+        while(!q.empty()){
+            pair<Node*,int> temp = q.front();
+            q.pop();
+            
+            Node* frontNode = temp.first;
+            int hd = temp.second;
+            
+            // for a hori. distance of a node just modify the mapping whenever found the node
+            topNode[hd] = frontNode->data;
+            
+            if(frontNode->left){
+                q.push(make_pair(frontNode->left,hd-1));
+            }
+            
+            if(frontNode->right){
+                q.push(make_pair(frontNode->right,hd+1));
+            }
+            
+        }
+        
+        for(auto i: topNode){
+            ans.push_back(i.second);
+        }
+        
+        return ans;
+    }
+
+};
