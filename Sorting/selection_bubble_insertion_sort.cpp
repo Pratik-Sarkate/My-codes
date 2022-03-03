f1e7cecf2c9122e1988fca4f7ae995d791043b95
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2022-03-04 01:27:15 +0530

    Selection, Bubble, Insertion sort code

--- Sorting/selection_bubble_insertion_sort.cpp
+++ Sorting/selection_bubble_insertion_sort.cpp
@@ -26,11 +26,16 @@ void insertion_sort(vector<int> &a, int n){
 void bubble_sort(vector<int> &a, int n){
 
     for(int i = 0; i<n-1; i++){
-        for(int j = i+1; j<n; j++){
-            if(a[i]>a[j]){
-                swap(a[i],a[j]);
+
+        bool flg = 0;
+        for(int j = 0; j<n-i-1; j++){
+            if(a[j]>a[j+1]){
+                swap(a[j],a[j+1]);
+                flg = 1;
             }
         }
+
+        if(!flg) break;
     }
 
     for(auto it: a) cout<<it<<" ";
