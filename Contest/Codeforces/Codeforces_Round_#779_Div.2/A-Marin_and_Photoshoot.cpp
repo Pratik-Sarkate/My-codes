8f06d0fe7009b0543f63f22baa69b7446287a163
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2022-03-27 23:24:39 +0530

    Upsolved A

---
+++ Contest/Codeforces/Codeforces_Round_#779_Div.2/A-Marin_and_Photoshoot.cpp
@@ -0,0 +1,47 @@
+// https://codeforces.com/contest/1658/problem/A
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define fast_io std::ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+using namespace std;
+
+
+void solve(){
+    int n;cin>>n;
+    string s;cin>>s;
+
+    int cnt = 0;
+    
+    int i = 0;
+    while(i<n){
+        if(s[i]=='0'){
+            if(i+1 < n && s[i+1]=='0') cnt+=2;
+            else if(i+2 < n && s[i+2]=='0'){
+                cnt+=1;
+                i++;
+            }
+        }
+        i++;
+    }
+    cout<<cnt<<endl;
+
+}
+
+int main()
+{
+    fast_io;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    // Code
+    int t = 1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
